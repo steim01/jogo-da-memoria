13eeb871bc128861efecae5159c80f4c377818ca
steim01 <dev.steim01@gmail.com>
2022-04-12 03:38:07 -0300

    foi feito todas as alterações nos arquivos para finalizar esse projeto

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,37 +4,20 @@ const BACK = "card_back";
 const CARD = "card";
 const ICON = "icon";
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'
-];
-
-let cards= null;
 
 startGame();
 
 // função para iniciar o jogo
 function startGame(){
 
-    cards = createCardsFromTechs(techs);
-    shuffleCards(cards);
-    initializeCards(cards);
+    initializeCards(game.createCardsFromTechs());
 }
 
 // função para pegar o medelo das cartas e deixar ela " virtual"
 function initializeCards(cards){
-
     let gameBoard = document.getElementById("gameBoard");
-
-    cards.forEach(card =>{
+    gameBoard.innerHTML = '';
+    game.cards.forEach(card =>{
 
         let cardElement = document.createElement('div');
         cardElement.id = card.id;
@@ -45,10 +28,7 @@ function initializeCards(cards){
 
         cardElement.addEventListener('click', flipCard);
         gameBoard.appendChild(cardElement);
-
-        
     })
-
 
 }
 
@@ -71,62 +51,44 @@ function createCardFace(face, card, element){
         iconElement.src = "./assets/images/" + card.icon + ".png";
         cardElementFace.appendChild(iconElement);
 
-    }else{
+    } else{
         cardElementFace.innerHTML = "&lt/&gt";
     }
     element.appendChild(cardElementFace);
 }
 
 
-// função para embaralhar as cartas
-function shuffleCards(cards){
-
-    let currentIndex = cards.length;
-    let randomIndex = 0;
-
-    while(currentIndex !== 0){
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]; // para inverter os valores
-    }
-
-}
-
-// função para criar as cartas
-function createCardsFromTechs(techs){
-
-    let cards =[];
-
-    techs.forEach((tech) => {
-        cards.push(createPairFromTech(tech));
-    })
-
-    return cards.flatMap(pair => pair);
-}
-
-// função para criar os pares de cartas
-function createPairFromTech(tech){
-
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }];
-}
- //função para criar um número aleatório para podermos trabalhar
-function createIdWithTech(tech){
-    
-    return tech + parseInt(Math.random() *1000);
-}
-
 // função para "virar" a carta
 function flipCard(){
-    this.classList.add("flip");
+
+    if (game.setCard(this.id)) {
+
+        this.classList.add("flip");
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+                if (game.checkGameOver()) {
+                    let gameOverLayer = document.getElementById("gameOver");
+                    gameOverLayer.style.display = 'flex';
+                }
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+
+            };
+        }
+    }
+}
+
+function restart() {
+    game.clearCards();
+    startGame();
+    let gameOverLayer = document.getElementById("gameOver");
+    gameOverLayer.style.display = 'none';
 }
